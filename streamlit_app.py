6ab89bbe49f0c72ae5dfee9d6e22d4fd5c5b9540
Oleksandr Pozden <95351527+OleksandrPozden@users.noreply.github.com>
2026-03-25 17:31:21 +0200

    Comment out dataframe display and stop in Streamlit app
    Comment out dataframe display and stop calls.

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 ingredients = st.multiselect(
     "Choose up to 5 ingredients",
@@ -38,6 +38,8 @@ if ingredients:
     for ingredient in ingredients:
         ingredients_string += ingredient + ' '
         st.subheader(f"Nutrition for: {ingredient}")
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + ingredient)  
         st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)  
     #st.text(ingredients_string)
